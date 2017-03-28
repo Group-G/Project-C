c04cfd4ed685748c904409304a5e7d46df5d0b7c
Alazar Genene <alazar.genene@gmail.com>
2017-03-28 12:37:57 -0400

    used connection.createStatement() functions to add sequel statements

--- src/groupg/JavaDBExample.java
+++ src/groupg/JavaDBExample.java
@@ -37,15 +37,15 @@ public class JavaDBExample
                 connection = DriverManager.getConnection("jdbc:derby:myDB;create=true");
                 Statement stmt = connection.createStatement();
 
-               /* String drop0 = "DROP TABLE ROOM";
-                String drop1 = "DROP TABLE DOCTOR";
+                String drop0 = "DROP TABLE ROOM";
+               /* String drop1 = "DROP TABLE DOCTOR";
                 String drop2 = "DROP TABLE CONNECTION";
                 String drop3 = "DROP TABLE FLOOR";
                 String drop4 = "DROP TABLE BUILDING";
                 String drop5 = "DROP TABLE LOCATION";
                 //stmt.execute(str0);
 */
-                String create0 = "CREATE TABLE ROOM (roomID CHAR(20) NOT NULL Primary Key)";
+                /*String create0 = "CREATE TABLE ROOM (roomID CHAR(20) NOT NULL Primary Key)";
                 //stmt.execute(str1);
 
                 String str1 = "INSERT INTO ROOM VALUES (11, 'Jim Bye', 'Buffalo', 34)";
@@ -53,17 +53,41 @@ public class JavaDBExample
                 stmt.execute(str1);
                 stmt.execute(str1);
 
+*/
 
-                String str3 = "SELECT * FROM CUSTOMER";
-                ResultSet res = stmt.executeQuery(str3);
+                connection.createStatement().execute("CREATE TABLE ROOM (roomID varchar(20) NOT NULL Primary Key)");
+               // connection.createStatement().execute("INSERT INTO ROOM VALUES (11, 'Jim Bye', 'Buffalo', 34)");
+                connection.createStatement().execute("INSERT INTO ROOM VALUES ('Jim Bye')");
+                //connection.createStatement().execute("DROP TABLE ROOM");
+                connection.createStatement().execute("CREATE TABLE CUSTOMER (SALARY varchar(20), NAME varchar(20), LOCATION varchar(20), AGE varchar(20))");
+                connection.createStatement().execute("INSERT INTO CUSTOMER VALUES ('111','yoo', 'Room 30', '20')");
+               // connection.createStatement().execute("DROP TABLE CUSTOMER");
+
+
+            //String str3 = "SELECT * FROM CUSTOMER";
+                Statement statement = connection.createStatement();
+                String SQLStatement = "SELECT * FROM CUSTOMER";
+                ResultSet res = stmt.executeQuery(SQLStatement);
+                ResultSetMetaData resultSetMetaData = res.getMetaData();
+                int columnCount = resultSetMetaData.getColumnCount();
+                for (int i = 1; i <= columnCount; i++){
+                System.out.format("%20s", resultSetMetaData.getColumnName(i) +'|');
+                 }
+
+
                 System.out.println(res);
                 while (res.next()) {
+                    System.out.println("");
+                    for (int j = 1; j <= columnCount; j++){
+                       System.out.format("%20s", res.getString(j) +'|');
+                    }
                     System.out.println(
                             "  "+res.getInt("SALARY")
                                     + ", "+res.getString("NAME")
                                     + ", "+res.getString("LOCATION")
                                     + ", "+res.getInt("AGE"));
                 }
+
             // substitute your database name for myDB
             connection = DriverManager.getConnection("jdbc:derby:myDB;create=true");
         }
