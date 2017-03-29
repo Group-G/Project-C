a985e6114e5696d5eb1b22cf2fc25bbadc16f3a4
Ryan Benasutti <firey45@gmail.com>
2017-03-28 22:39:49 -0400

    Complete UI for Project C (#1)
    * added initial Admin login page
    
    * added files
    
    * added jackalope pic and moved adminLogin to res
    
    * Updated build status badge
    
    * Added .iml to gitignore
    
    * Updated readme for this branch
    
    * Linked welcomeScreen and adminLogin
    
    * Added back button, changed admin login button to open in same scene
    
    * Fixed prompt text
    
    * added two new windows "adminMain", and "editCoordinates".
    
    * Added adminMain
    
    * Bound adminLoginUsernameField, adminLoginPasswordField
    
    * Added to gitignore
    
    * added "editCategory"
    
    * Added remaining functionality for editCategory, editCoordinates
    
    * added "editCategory"
    
    * Added functionality to editCoordinate logout btn
    
    * Renamed for shorter fields
    
    * Removed unused menubtn item on welcomeScreen
    
    * Added directions screen and map image
    
    * Added bindings for directionScreen, removed sample.fxml

--- src/groupg/Main.java
+++ src/groupg/Main.java
@@ -6,18 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
+public class Main extends Application
+{
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("/welcomeScreen.fxml"));
+        primaryStage.setTitle("Welcome");
+        primaryStage.setScene(new Scene(root, 610, 400));
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
